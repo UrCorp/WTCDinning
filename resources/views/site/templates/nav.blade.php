0620ab106f7b0fa8f87031f1193118670e5ef969
Eduardo Vera <eduardo.vera.pineda@gmail.com>
2017-04-25 13:37:26 -0500

    [FrontEnd] Sintaxis menu

--- resources/views/site/templates/nav.blade.php
+++ resources/views/site/templates/nav.blade.php
@@ -2,7 +2,7 @@
     <div class="nav-wrapper">
       <img src="{{ asset('img/wtc-menu.png') }}" alt="">
       <ul class="right hide-on-med-and-down">
-        <li><a href="#>Home</a></li>
+        <li><a href="#">Home</a></li>
         <li><a href="Dinning">Servicios</a></li>
         <li><a href="#" title="Real State">Real Estate</a></li>
         <li><a href="#">Tienda</a></li>
