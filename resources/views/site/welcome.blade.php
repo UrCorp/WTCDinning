6bd19c07b040fdd37a7809c564b52cf04687c143
Eduardo Vera <eduardo.vera.pineda@gmail.com>
2017-06-26 15:38:23 -0500

    [FrontEnd:Home] First change to Pooky's design

--- resources/views/site/welcome.blade.php
+++ resources/views/site/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('body-content')
 
-<div class="slider grey lighten-4 w3-container-display" style="">
+<div class="slider w3-container-display" style="">
   <ul class="slides valign-wrapper">
     <li>
       <img src="{{ asset('img/home/slider/momentum.png') }}">
@@ -50,107 +50,83 @@
   <button class="next btn-flat white-text w3-display-right" style="z-index: 5; height: 50px; font-size: 36px">&#10095;</button>
 </div>
 
-<div class="grey lighten-4">
+<!-- Academics -->
+<div class="grey lighten-3">
   <div class="container">
-  <div class="section">
-    <h3 class="center">Explore World Trade Center</h3>
-  </div>
-  <div class="section">
-    <div class="row">
-      <br>
-      <div class="col s12 m4">
-        <div class="card">
-          <div class="card-image">
-            <img src="{{ asset('img/home/cardRealEstate.jpg') }}">
+    <div class="section">      
+      <h3 class="grey-text text-darken-2">Academics</h3>
+    </div>
+    <div class="section">
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="w3-card">
+            <img src="{{ asset('img/home/a-certification.jpg') }}" alt="Norway" width="100%">
+            <div class="w3-container white w3-padding">
+              <h5 class="w3-center grey-text text-darken-2">WTC CERTIFICATION</h5>
+              <p class="w3-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+              <a class="waves-effect waves-light grey darken-2 w3-button w3-block white-text" style="border: solid 1px black; border-color: white" href="#modal1" >
+                INCRIBIRME
+              </a>
+            </div>
           </div>
         </div>
-        <p class="promo-caption center">Real Estate</p>
-        <p class="light center">Discover our exclusive corporate and medical complex, surrounded by commercial areas, persuading investment in the Bajio Region.</p>
-      </div>
-      <div class="col s12 m4">
-        <div class="card">
-          <div class="card-image">
-            <img src="{{ asset('img/home/cardSoftLanding.jpg') }}">
+        <div class="col s12 m4">
+          <div class="w3-card">
+            <img src="{{ asset('img/home/a-lean6sigma.jpg') }}" alt="Norway" width="100%">
+            <div class="w3-container white w3-padding">
+              <h5 class="w3-center grey-text text-darken-2">LEAN SIX SIGMA</h5>
+              <p class="w3-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+              <a class="waves-effect waves-light grey darken-2 w3-button w3-block white-text" style="border: solid 1px black; border-color: white" href="#modal1" >
+                INCRIBIRME
+              </a>
+            </div>
           </div>
         </div>
-        <p class="promo-caption center">Softlanding</p>
-        <p class="light center">WTC guides you systematically to establish your company in Mexico; we take care of everything so you can dedicate to grow your business.</p>
-      </div>
-      <div class="col s12 m4">
-        <div class="card">
-          <div class="card-image">
-            <img src="{{ asset('img/home/cardTradeMissions.jpg') }}">
+        <div class="col s12 m4">
+          <div class="w3-card">
+            <img src="{{ asset('img/home/a-university.jpg') }}" alt="Norway" width="100%">
+            <div class="w3-container white w3-padding">
+              <h5 class="w3-center grey-text text-darken-2">WTC UNIVSERSITY</h5>
+              <p class="w3-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+              <a class="waves-effect waves-light grey darken-2 w3-button w3-block white-text" href="#modal1" >
+                INCRIBIRME
+              </a>
+            </div>
           </div>
         </div>
-        <p class="promo-caption center">Speeds up development</p>
-        <p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
       </div>
-    </div>
-    </div>
-  </div>
-</div>
-
-<div class="container center-align">
-  <div class="section">
-    <div class="row">
-        <h4 class="center">Let's talk about business</h4>
-        <p class="col s6 offset-s3 center">World Trade Center is a global platform that promotes international trade, with a Supplier Network located in the most important cities in the world.</p>
-    </div>
-    <div class="video-container center">
-      <iframe  width="854" height="480" src="https://www.youtube.com/embed/cHr360f6naM" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div class="row center">
-      <br>
-      <br>
-      <a class="waves-effect waves-light btn-flat blue-text"> Program your virtual consultancy</a>
-      <br>
-      <br>
-      <br>
     </div>
   </div>
 </div>
+<!-- End Academics -->
 
+<!-- WTC Landing -->
 <div class="wrapper">
-  <div id="index-banner" class="parallax-container" style="height: 300px;">
-    <div class="parallax grey lighten-4 valign-wrapper"><img src="{{ asset('img/home/arcos.png') }}" alt="Unsplashed background img 1"></div>
-      <div class="caption center-align hide-on-med-and-down">
-        <h3 class="black-text">QUERÉTARO</h3>
-        <div class="row">
-          <div class="col s12 m7 offset-m3">
-            <div class="black-text text-lighten-3">Named several times as the safest and best life quality city in the country.</div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col s7 offset-s3">
-            <div class="black-text text-lighten-3"> Discover the main activities in the fastest growing city in Latin America.</div>
-          </div>
-        </div>
+  <div id="index-banner" class="parallax-container valign-wrapper" style="height: 600px;">
+    <div class="parallax valign-wrapper"><img src="{{ asset('img/home/landing.jpg') }}" alt="Unsplashed background img 1"></div>
+      <div class="caption left-align hide-on-med-and-down" style="padding-left: 10%">
+          <img src="{{ asset('img/home/wtc-landing.png') }}" width="50%">
+          <br>
+          <h5 class="light grey-text text-lighten-3">¡TU FUTURO ESTA SEGURO CON NOSOTROS!</h5>
+
+          <a class="waves-effect waves-light white w3-btn pink-text text-darken-2 w3-padding-16" href="#modal1" >
+            LEER MAS
+          </a>
       </div>
       <div class="caption center-align hide-on-large-only">
-        <h3 class="black-text">QUERÉTARO!</h3>
-        <div class="row">
-          <div class="col s12 m7 offset-m3">
-            <div class="black-text text-lighten-3">Named several times as the safest and  </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col s12 m7 offset-m3">
-            <div class="black-text text-lighten-3">sbest life quality city in the country.</div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col s12 m7 offset-m3">
-            <div class="black-text text-lighten-3">Discover the main activities in the </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col s12 m7 offset-m3">
-            <div class="black-text text-lighten-3">fastest growing city in Latin America.</div>
-          </div>
-        </div>
       </div>
   </div>  
 </div>
+<!-- End WTC Landing -->
 
 <div id="index-banner" class="parallax-container" style="height: 300px;">
   <div class="parallax"><img src="{{ asset('img/home/celphone.jpg') }}" alt="Unsplashed background img 1"></div>
