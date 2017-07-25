77a316830164fd5a4467f615c243ef436bfc7e01
Eduardo Vera <eduardo.vera.pineda@gmail.com>
2017-07-25 14:50:04 -0500

    [FrontEnd:LeanSixSigma] Layer color for the little image added

--- resources/views/site/leansixsigma.blade.php
+++ resources/views/site/leansixsigma.blade.php
@@ -52,20 +52,32 @@
 <div class="container hide-on-small-only" style="margin-top: 5%; margin-bottom: 5%">
   <div class="w3-row-padding">
     <div class="w3-col s4 w3-container">
-      <img src="{{ asset('img/lean6sigma/YB.jpg') }}" style="width: 100%">
+      <div class="w3-display-container">
+        <img src="{{ asset('img/lean6sigma/YB.jpg') }}" style="width: 100%">
+        <div class="w3-display-middle" style="background-color: rgba(229, 211, 50, 0.8);position: absolute;width: 100%;height: 100%;"></div>
+        <h5 class="w3-display-topleft" style="color: rgba(255, 255, 255, 0.9); font-weight:900">Yellow Belt <br> Course </h5>
+      </div>
       <p class="w3-justify font-gray letra-chica" style="position: relative;">
         Focuses on preparing individuals to develop efficient processes for fast delivery and consistent quality.
       </p>
     </div>
     <div class="w3-col s4 w3-container" >
-      <img src="{{ asset('img/lean6sigma/GB.jpg') }}" style="width: 100%">
+      <div class="w3-display-container">
+        <img src="{{ asset('img/lean6sigma/GB.jpg') }}" style="width: 100%">
+        <div class="w3-display-middle" style="background-color: rgba(171, 208, 58, 0.8);position: absolute;width: 100%;height: 100%;"></div>
+        <h5 class="w3-display-topleft" style="color: rgba(255, 255, 255, 0.9); font-weight:900">Green Belt <br> Course </h5>
+      </div>
       <p class="w3-justify font-gray letra-chica">
         Green belts are agents of change trained in the methodologies of Lean Six Sigma, and as such, are capable
         of implementing high-impact projects.
       </p>
     </div>
     <div class="w3-col s4 w3-container">
-      <img src="{{ asset('img/lean6sigma/BB.jpg') }}" style="width: 100%">
+      <div class="w3-display-container">
+        <img src="{{ asset('img/lean6sigma/BB.jpg') }}" style="width: 100%">
+        <div class="w3-display-middle" style="background-color: rgba(0, 0, 0, 0.8);position: absolute;width: 100%;height: 100%;"></div>
+        <h5 class="w3-display-topleft" style="color: rgba(255, 255, 255, 0.9); font-weight:900">Black Belt <br> Course </h5>
+      </div>
       <p class="w3-justify font-gray letra-chica">
         Black Belts are experts in Lean Six Sigma methodologies and dedicate much of their time implementing
         company improvements, leading key projects, and training or mentoring staff.
@@ -86,14 +98,22 @@
 <div class="container hide-on-med-and-up" style="margin-top: 5%">
   <div class="w3-row-padding">
     <div class="w3-col s12 w3-container">
-      <img src="{{ asset('img/lean6sigma/YB.jpg') }}" style="width: 100%">
+      <div class="w3-display-container">
+        <img src="{{ asset('img/lean6sigma/YB.jpg') }}" style="width: 100%">
+        <div class="w3-display-middle" style="background-color: rgba(229, 211, 50, 0.8);position: absolute;width: 100%;height: 100%;"></div>
+        <h5 class="w3-display-topleft" style="color: rgba(255, 255, 255, 0.9); font-weight:900">Yellow Belt <br> Course </h5>
+      </div>
       <p class="w3-justify font-gray letra-chica" style="position: relative;">
         Focuses on preparing individuals to develop efficient processes for fast delivery and consistent quality.
       </p>
       <a href="#" class="w3-btn btnlean">See Calendar</a>
     </div>
     <div class="w3-col s12 w3-container" >
-      <img src="{{ asset('img/lean6sigma/GB.jpg') }}" style="width: 100%">
+      <div class="w3-display-container">
+        <img src="{{ asset('img/lean6sigma/GB.jpg') }}" style="width: 100%">
+        <div class="w3-display-middle" style="background-color: rgba(171, 208, 58, 0.8);position: absolute;width: 100%;height: 100%;"></div>
+        <h5 class="w3-display-topleft" style="color: rgba(255, 255, 255, 0.9); font-weight:900">Green Belt <br> Course </h5>
+      </div>
       <p class="w3-justify font-gray letra-chica">
         Green belts are agents of change trained in the methodologies of Lean Six Sigma, and as such, are capable
         of implementing high-impact projects.
@@ -101,7 +121,11 @@
       <a href="#" class="w3-btn btnlean">See Calendar</a>
     </div>
     <div class="w3-col s12 w3-container">
-      <img src="{{ asset('img/lean6sigma/BB.jpg') }}" style="width: 100%">
+      <div class="w3-display-container">
+        <img src="{{ asset('img/lean6sigma/BB.jpg') }}" style="width: 100%">
+        <div class="w3-display-middle" style="background-color: rgba(0, 0, 0, 0.8);position: absolute;width: 100%;height: 100%;"></div>
+        <h5 class="w3-display-topleft" style="color: rgba(255, 255, 255, 0.9); font-weight:900">Black Belt <br> Course </h5>
+      </div>
       <p class="w3-justify font-gray letra-chica">
         Black Belts are experts in Lean Six Sigma methodologies and dedicate much of their time implementing
         company improvements, leading key projects, and training or mentoring staff.
