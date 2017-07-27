9ff17751dc9461936467be932294b4671f46cb26
Eduardo Vera <eduardo.vera.pineda@gmail.com>
2017-07-27 15:09:04 -0500

    [FrontEnd:LeandSixSigma] Social Media icons fixed

--- resources/views/site/es/leansixsigma.blade.php
+++ resources/views/site/es/leansixsigma.blade.php
@@ -11,14 +11,14 @@
     <div class="parallax valign-wrapper">
       <img src="{{ asset('img/lean6sigma/lean.jpg') }}" alt="">
     </div>
-    <div class="w3-display-right" style="margin: -90px 200px 0 0">
+    <div class="w3-display-right hide-on-small-only" style="margin: -90px 200px 0 0;">
       <a href="#" class="fa fa-facebook socialnav"></a>
-      <a href="#" class="fa fa-instagram socialnav"></a>
+      <a href="#" class="fa fa-instagram socialnav" style="margin: 10px"></a>
       <a href="#" class="fa fa-twitter socialnav"></a>
     </div>
     <div class="caption left-align hide-on-small-only" style="z-index: 100; padding-left: 5%; width: 40%">
         <h3 class="light grey-text text-lighten-3"><b>LEAN SIX SIGMA</b></h3>
-        <p class="w3-justifylean6sigma">
+        <p class="w3-justify">
           Mejore el desempeño operacional aprovechando diversas herramientas para identificar y eliminar defectos. Nuestra formación tiene aplicaciones
           directas y tangibles en casi todos los procesos de fabricación.
         </p>
